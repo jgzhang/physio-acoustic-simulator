0655d5e483de20e37b9236cacf1e263e93ef3928
Rob McClure <mgracecubs@gmail.com>
2011-02-28 16:02:04 -0800

    Adding Scott's partition functions.

---
+++ partition.m
@@ -0,0 +1,25 @@
+function out = partition( count)
+%PARTITION Summary of this function goes here
+%   outputs list of partitions that increase in size exponentially.
+%   if partition one is of size 1, the following partitions will be of size
+%   2,3,4,5 etc.
+if(size(count) ~= 1)
+    error('variable count must be a single integer)');
+end
+
+
+sum = 0;
+for i=1:count;
+    sum = sum + i;
+end
+
+temp = 10000 / sum;
+
+out = [];
+
+sum = 0;
+
+for i=1:count;
+    out = [out;sum (sum + i*temp)];
+    sum = sum + i*temp;
+end
